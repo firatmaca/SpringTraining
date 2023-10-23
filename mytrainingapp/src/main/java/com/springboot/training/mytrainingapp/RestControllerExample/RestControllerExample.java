6a78c29154f480624d1654e5c8a70c6f55853f46
FIRAT ATMACA <firat.atmaca@bites.com.tr>
2023-10-24 00:09:14 +0300

    SPRING rest controller example

---
+++ mytrainingapp/src/main/java/com/springboot/training/mytrainingapp/RestControllerExample/RestControllerExample.java
@@ -0,0 +1,13 @@
+package com.springboot.training.mytrainingapp.RestControllerExample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerExample {
+
+    @GetMapping
+    public String getSayHello(){
+        return "Hellooo";
+    }
+}
